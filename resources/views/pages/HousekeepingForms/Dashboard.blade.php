a9b1c24c386a587f921c215f5821348ee6e74d6b
DanielXD0207 <daniel.tolibas.diapen@gmail.com>
2022-11-30 19:32:48 +0800

    Housekeeping Dashboard and Room Management Updated

--- resources/views/pages/HousekeepingForms/Dashboard.blade.php
+++ resources/views/pages/HousekeepingForms/Dashboard.blade.php
@@ -59,10 +59,10 @@
                     <div class="card-header border-0">
                         <div class="row align-items-center">
                             <div class="col">
-                                <h3 class="mb-0">Sample </h3>
+                                <h3 class="mb-0">Room Assignment </h3>
                             </div>
                             <div class="col text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                                <!--<a href="#!" class="btn btn-sm btn-primary">See all</a>-->
                             </div>
                         </div>
                     </div>
@@ -71,29 +71,28 @@
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
                                 <tr>
-                                    <th scope="col">Sample Field</th>
-                                    <th scope="col">Sample Field</th>
-                                    <th scope="col">Sample Field</th>
-                                    <th scope="col">Sample Field</th>
+                                    <th scope="col">Due Date</th>
+                                    <th scope="col">Room</th>
+                                    <th scope="col">Housekeeping Status</th>
+                                    <th scope="col">Booking Status</th>
+                                    <th scope="col">Attendant</th>
+                                    <th scope="col">Arrival</th>
+                                    <th scope="col">Departure</th>
+                                    <th scope="col">Guest Preference</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>Sample Data</td>
-                                    <td>Sample Data</td>
-                                    <td>Sample Data</td>
+                                    <td><?php echo date("M-d-Y");?></td>
+                                    <td>101</td>
+                                    <td>Cleaned</td>
+                                    <td>Available</td>
+                                    <td>Kay</td>
+                                    <td><?php echo date("M-d-Y");?></td>
+                                    <td><?php echo date("M-d-Y");?></td>
                                     <td>
                                         <!--<i class="fas fa-arrow-up text-success mr-3"></i> 46,53%-->
-                                        Sample Data
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Sample Data</td>
-                                    <td>Sample Data</td>
-                                    <td>Sample Data</td>
-                                    <td>
-                                        <!--<i class="fas fa-arrow-up text-success mr-3"></i> 46,53%-->
-                                        Sample Data
+                                        Dark Curtains
                                     </td>
                                 </tr>
                             </tbody>
--- resources/views/pages/HousekeepingForms/RoomManagement.blade.php
+++ resources/views/pages/HousekeepingForms/RoomManagement.blade.php
@@ -14,7 +14,7 @@
                                 <h3 class="mb-0">Room Management</h3>
                             </div>
                             <div class="col text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                                <!--<a href="#!" class="btn btn-sm btn-primary">See all</a>-->
                             </div>
                         </div>
                     </div>
@@ -33,47 +33,72 @@
                                 <tr>
                                     <td>101</td>
                                     <td>Family Room</td>
+                                    <td>Checked</td>
+                                    <td>High</td>
                                     <td>
-                                        <select class="form-control" value="" style="width:auto;">
-                                            <option value="Available">Available</option>
-                                            <option value="Dirty">Dirty</option>
-                                            <option value="Checked">Checked</option>
-                                            <option value="Cleaning">Cleaning</option>
-                                            <option value="Out of Service">Out of Service</option>
-                                            <option value="Out of Order">Out of Order</option>
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <select class="form-control" style="width:auto;">
-                                            <option value="Low">Low</option>
-                                            <option value="Moderate">Moderate</option>
-                                            <option value="High">High</option>
-                                            <option value="Top Priority">Top Priority</option>
-                                        </select>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                            Action
+                                        </button>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>102</td>
                                     <td>Single Room</td>
+                                    <td>Available</td>
+                                    <td>Low</td>
                                     <td>
-                                        <select class="form-control" value="" style="width:auto;">
-                                            <option value="Available">Available</option>
-                                            <option value="Dirty">Dirty</option>
-                                            <option value="Checked">Checked</option>
-                                            <option value="Cleaning">Cleaning</option>
-                                            <option value="Out of Service">Out of Service</option>
-                                            <option value="Out of Order">Out of Order</option>
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <select class="form-control" style="width:auto;">
-                                            <option value="Low">Low</option>
-                                            <option value="Moderate">Moderate</option>
-                                            <option value="High">High</option>
-                                            <option value="Top Priority">Top Priority</option>
-                                        </select>
+                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                            Action
+                                        </button>
                                     </td>
                                 </tr>
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title text-left display-4" id="exampleModalLabel">Manage Room</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="row">
+                                                    <div class="card-body bg-white" style="border-radius: 18px">
+                                                        <p class="text-left">Room: </p>
+                                                        <input class="form-control" type="text" value="1" id="example-datetime-local-input" readonly>
+                                                        
+                                                        <p class="text-left">Room Type: </p>
+                                                        <input class="form-control" type="text" value="Family Room" id="example-datetime-local-input" required>
+                                                        
+                                                        <p class="text-left">Housekeeping Status </p>
+                                                        <select class="form-control">
+                                                            <option value="Available">Available</option>
+                                                            <option value="Dirty">Dirty</option>
+                                                            <option value="Checked">Checked</option>
+                                                            <option value="Cleaning">Cleaning</option>
+                                                            <option value="Out of Service">Out of Service</option>
+                                                            <option value="Out of Order">Out of Order</option>
+                                                        </select>
+                                                        
+                                                        <p class="text-left">Priority </p>
+                                                        <select class="form-control">
+                                                            <option value="Low">Low</option>
+                                                            <option value="Moderate">Moderate</option>
+                                                            <option value="High">High</option>
+                                                            <option value="Top Priority">Top Priority</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-success">Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </tbody>
                         </table>
                     </div>
