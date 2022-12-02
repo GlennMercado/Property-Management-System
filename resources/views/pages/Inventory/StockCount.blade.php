76003aa12c060c4e0fd1fffd75e97261f33d25bd
Rraine Autor <Rraine@gmail.com>
2022-12-02 17:30:59 +0800

    Update: Stock Count with modal and validations

--- resources/views/pages/Inventory/StockCount.blade.php
+++ resources/views/pages/Inventory/StockCount.blade.php
@@ -11,9 +11,12 @@
                         <div class="row align-items-center">
                             <div class="col">
                                 <div class = "col">
-                                    <a  href="{{ route('CreateInventory') }}" class = "btn btn-primary" style = "float:right;">
+                                    <!--<a  href="{{ route('CreateInventory') }}" class = "btn btn-primary" style = "float:right;">
                                     Create Inventory
-                                    </a>
+                                    </a>-->
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" style = "float:right;">
+                                            Action
+                                        </button>
                                 </div>
                                 <h3 class="mb-0">Stocks</h3>
                             </div>
@@ -26,8 +29,8 @@
                                 <tr>
                                     <th scope="col">Product Code</th>
                                     <th scope="col">Item Name</th>
-                                    <th scope="col">Supplier</th>
-                                    <th scope="col">Description</th>
+                                    <th scope="col">Details</th>
+                                    <th scope="col">Type</th>
                                     <th scope="col">Available Stock</th>
                                     <th scope="col">Action</th>
                                 </tr>
@@ -40,8 +43,9 @@
                                     <td>Sample Data</td>
                                     <td>Sample Data</td>
                                     <td>
-                                        <input type="submit" class="bi-eye btn-primary" id=""  value="View"></input> ||
-                                        <input type="submit" class="bi bi-pencil-square btn-primary" id=""  value="Edit"></input>
+                                        <a href = "#"><i class="bi bi-eye"></i></a>
+                                        <a href = "#"><i class="bi bi-pencil-square"></i></a>
+                                        <a href = "#"><i class="bi bi-x-circle"></i></a>
                                     </td>
                                 </tr>
                                 <tr>
@@ -51,8 +55,33 @@
                                     <td>Sample Data</td>
                                     <td>Sample Data</td>
                                     <td>
-                                        <input type="submit" class="bi-eye btn-primary" id=""  value="View"></input> ||
-                                        <input type="submit" class="bi bi-pencil-square btn-primary" id=""  value="Edit"></input>
+                                        <a href = "#"><i class="bi bi-eye"></i></a>
+                                        <a href = "#"><i class="bi bi-pencil-square"></i></a>
+                                        <a href = "#"><i class="bi bi-x-circle"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>
+                                        <a href = "#"><i class="bi bi-eye"></i></a>
+                                        <a href = "#"><i class="bi bi-pencil-square"></i></a>
+                                        <a href = "#"><i class="bi bi-x-circle"></i></a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>Sample Data</td>
+                                    <td>
+                                        <a href = "#"><i class="bi bi-eye"></i></a>
+                                        <a href = "#"><i class="bi bi-pencil-square"></i></a>
+                                        <a href = "#"><i class="bi bi-x-circle"></i></a>
                                     </td>
                                 </tr>
                             </tbody>
@@ -64,6 +93,76 @@
 
             @include('layouts.footers.auth')
         </div>
+    <!-- Modal -->
+    
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-left display-4" id="exampleModalLabel">Create Stocks</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                    </div>
+                        <div class="modal-body">
+                            <form class="needs-validation" novalidate>
+                                <p class="text-left">Stock ID: </p>
+                                    <input class="form-control" type="text" value="1" id="example-datetime-local-input" readonly>
+                        <div class="form-group">
+                            <label for="Stockname">Stock Name</label>
+                                <input type="text" class="form-control" id="Stockname" aria-describedby="emailHelp" placeholder="Enter name..." required>
+                        <div class="invalid-feedback">
+                        Stock Name empty
+                        </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="Stockdetails">Stock Details</label>
+                                <input type="text" class="form-control" id="Stockdetails" placeholder="Enter details..." required>
+                                    <div class="invalid-feedback">
+                                    Stock Details empty
+                                    </div>
+                </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Stock Type</label>
+                            <select class="form-control" required>
+                                <option value="Stock1">Sample 1</option>
+                                <option value="Stock2">Sample 2</option>
+                                <option value="Stock3">Sample 3</option>
+                            </select>
+                                <div class="invalid-feedback">
+                                Stock Details empty
+                                </div>
+                    </div>              
+            </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+            </div>
+        </div>
+    </div>                              
+  
+    <!--Validation-->                                 
+       <script>
+                 
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {  
+            var forms = document.getElementsByClassName('needs-validation');
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+                }, false);
+            });
+            }, false);
+        })();
+                    
+       </script>
+       
     @endsection
 
     @push('js')
