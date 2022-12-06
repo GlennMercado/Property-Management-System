23e0424b883974679282d5f5a5654948147debb4
maalqoh14 <glennlainardmercado@gmail.com>
2022-12-06 13:27:29 +0800

    Reservation

---
+++ app/Http/Controllers/HotelController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\hotel_reservations;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('pages.Reservations.HotelReservationForm');
+    }
+     
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'checkIn' => 'required',
+            'checkOut' => 'required',
+            'gName' => 'required',
+            'address' => 'required',
+            'mobile' => 'required'
+        ]);
+
+        $maintain = new hotel_reservations;
+
+        $maintain->check_in_date = $request->input('checkIn');
+        $maintain->check_out_date = $request->input('checkOut');
+        $maintain->guest_name = $request->input('gName');
+        $maintain->address = $request->input('address');
+        $maintain->mobile_num = $request->input('mobile');
+
+        $maintain->save();
+
+        return redirect('HotelReservationForm')->with('Success', 'Data Saved');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_12_06_000949_create_hotel_reservation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotel_reservations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('check_in_date');
+            $table->string('check_out_date');
+            $table->string('guest_name');
+            $table->string('address');
+            $table->integer('mobile_num');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotel_reservations');
+    }
+};
