19ea42bc19866c5eb0c0b6bb6aa09d0156a4f2e0
maalqoh14 <glennlainardmercado@gmail.com>
2022-12-15 16:17:11 +0800

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -176,6 +176,9 @@
         </div>
     </div>
     <div class="container-fluid bg-white" id="functionRooms">
+        <br>
+        <br>
+        <br>
         <div class="row">
             <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
                 <img src="{{ asset('nvdcpics') }}/FunctionRoom1.jpg" class="w-100 shadow-1-strong rounded mb-4" />
