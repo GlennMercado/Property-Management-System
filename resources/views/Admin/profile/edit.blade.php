fdb0a6be5431f12333ae7b2c4f85c81941ab9a5a
maalqoh14 <glennlainardmercado@gmail.com>
2023-01-24 17:48:43 +0800

    home ui

--- resources/views/Admin/profile/edit.blade.php
+++ resources/views/Admin/profile/edit.blade.php
@@ -13,7 +13,7 @@
                         <div class="col-lg-3 order-lg-2">
                             <div class="card-profile-image">
                                 <a href="#">
-                                    <img src="{{ asset('nvdcpics') }}/Glenn1.jpg" class="rounded-circle">
+                                    <img src="{{ asset('nvdcpics') }}/user.png" class="rounded-circle">
                                 </a>
                             </div>
                         </div>
--- resources/views/layouts/navbars/navs/guestloggedin.blade.php
+++ resources/views/layouts/navbars/navs/guestloggedin.blade.php
@@ -1,12 +1,12 @@
-<nav class="navbar fixed-top navbar-horizontal bg-white navbar-expand-md">
+<nav class="navbar fixed-top navbar-horizontal navbar-expand-md" style="background-color: #30bc6c">
     <div class="container px-2">
         <img src="{{ asset('nvdcpics') }}/nvdc-logo.png" style="width: 50px; height: 50px; margin-right:1%;">
         <a class="navbar-brand" href="{{ route('welcome') }}">
-            <h2 class="text-default">NVDC Properties</h2>
+            <h2 class="text-white">NVDC Properties</h2>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="ni ni-bullet-list-67 text-default"></span>
+            <span class="ni ni-bullet-list-67 text-white"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar-collapse-main">
             <!-- Collapse header -->
@@ -29,19 +29,19 @@
             <!-- Navbar items -->
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon text-default" href="{{ url('about_us') }}">
+                    <a class="nav-link nav-link-icon text-white font-weight-bold" href="{{ url('about_us') }}">
                         <i class="ni ni-single-02"></i>
                         <span class="nav-link-inner--text">{{ __('About Us') }}</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon text-default" href="{{ url('contact_us') }}">
+                    <a class="nav-link nav-link-icon text-white font-weight-bold" href="{{ url('contact_us') }}">
                         <i class="ni ni-single-02"></i>
                         <span class="nav-link-inner--text">{{ __('Contact Us') }}</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link nav-link-icon text-default" href="{{ url('map')}}">
+                    <a class="nav-link nav-link-icon text-white font-weight-bold" href="{{ url('map')}}">
                         <i class="ni ni-pin-3"></i>
                         <span class="nav-link-inner--text">{{ __('Map') }}</span>
                     </a>
@@ -62,10 +62,10 @@
                 aria-expanded="false">
                 <div class="media align-items-center">
                     <span class="avatar avatar-sm rounded-circle">
-                        <img alt="Image placeholder" src="{{ asset('nvdcpics') }}/Glenn1.jpg">
+                        <img src="{{ asset('nvdcpics') }}/user.png" class="rounded-circle">
                     </span>
                     <div class="media-body ml-2 d-none d-lg-block">
-                        <span class="mb-0 text-sm  font-weight-bold">{{ auth()->user()->name }}</span>
+                        <span class="mb-0 text-white font-weight-bold">{{ auth()->user()->name }}</span>
                     </div>
                 </div>
             </a>
