2a4b3e639e04569d7d2cbf1da8e51b45826113f3
PALCE <palcejohncarl12@gmail.com>
2023-01-31 17:04:58 +0800

    Finance module and the previous module

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -210,13 +210,6 @@
                             <i class="bi bi-person-lines-fill text-success"></i> {{ __('Operations Management') }}
                         </a>
                     </li>
-                    <!--Finance-->
-                    <li class="nav-item">
-                    <a class="nav-link text-default" href="{{ route('Finance') }}">
-                        <i class="bi bi-person-lines-fill text-success"></i> {{ __('Finance') }}
-                    </a>
-                </li>
-
             </ul>
             <!-- Divider -->
             <hr class="my-3">
