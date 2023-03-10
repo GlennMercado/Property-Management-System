d12ba8eba66d81f2d8ba926a5ba4d78ec398f20a
Rraine Autor <Rraine@gmail.com>
2023-03-10 09:48:27 +0800

    Om db, hk db

--- resources/views/Admin/pages/OperationManagement/OperationDashboard.blade.php
+++ resources/views/Admin/pages/OperationManagement/OperationDashboard.blade.php
@@ -26,7 +26,7 @@
                         <div class="row mt-3">
                             <div class="col-md-2">
                                 <div class="card">
-                                    <div class="card-body-md rounded" style="background-color:#156C45;">
+                                    <div class="card-body-md rounded" style="background-color:#2AD587;">
                                         <h1 class="text-secondary mx-auto d-flex justify-content-center mt-2">
                                             @foreach($request_count as $count)
                                                 {{$count->cnt}}
@@ -40,7 +40,7 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="card">
-                                    <div class="card-body-md rounded" style="background-color:#23B271;">
+                                    <div class="card-body-md rounded" style="background-color:#2FCF92;">
                                         <h1 class="text-secondary mx-auto d-flex justify-content-center mt-2">200</h1>
                                         <h5 class="text-secondary mx-auto d-flex justify-content-center text">Guest
                                             Reservation
@@ -50,7 +50,7 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="card">
-                                    <div class="card-body-sm rounded" style="background-color:#38474D;">
+                                    <div class="card-body-sm rounded" style="background-color:#34C99D;">
                                         <h1 class="text-secondary mx-auto d-flex justify-content-center mt-2">20</h1>
                                         <h5 class="text-secondary mx-auto d-flex justify-content-center text">Guest
                                             Reviews
@@ -60,7 +60,7 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="card">
-                                    <div class="card-body-sm rounded" style="background-color:#38474D;">
+                                    <div class="card-body-sm rounded" style="background-color:#39C3A8;">
                                         <h1 class="text-secondary mx-auto d-flex justify-content-center mt-2">
                                             @foreach($room1 as $count)
                                                 {{$count->cnt}}
@@ -74,7 +74,7 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="card">
-                                    <div class="card-body-sm rounded" style="background-color:#38474D;">
+                                    <div class="card-body-sm rounded" style="background-color:#3EBDB3;">
                                         <h1 class="text-secondary mx-auto d-flex justify-content-center mt-2">
                                             @foreach($room2 as $count)
                                                 {{$count->cnt}}
@@ -88,7 +88,7 @@
                             </div>
                             <div class="col-md-2">
                                 <div class="card">
-                                    <div class="card-body-sm rounded" style="background-color:#38474D;">
+                                    <div class="card-body-sm rounded" style="background-color:#43B7BE;">
                                         <h1 class="text-secondary mx-auto d-flex justify-content-center mt-2">
                                             @foreach($room3 as $count)
                                                 {{$count->cnt}}
