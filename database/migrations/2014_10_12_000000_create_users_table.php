25697cd503b7d8d945453a1ab908b84288be032b
DanielXD0207 <daniel.tolibas.diapen@gmail.com>
2023-04-05 18:03:45 +0800

    user create fixed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -25,11 +26,12 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
         DB::table('users')->insert(
             array(
                 'name' => 'SUPER ADMIN',
                 'email' => 'admin@gmail.com',
-                'password' => 'qweqweqwe',
+                'password' => Hash::make('qweqweqwe'),
                 'User_Type' => 'Admin',
             )
         );
