a40ee0905096b8c783de7a5877f95c9ae53687bc
maalqoh14 <glennlainardmercado@gmail.com>
2023-04-16 07:52:09 +0800

    finance_connection almost

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -147,12 +147,15 @@ class HotelController extends Controller
 
                 //For FinanceModule Variables
                 $ornum = "001";
-                $finance_payee = DB::select("SELECT gcash_account_name FROM hotel_reservations");
+                // $finance_payee = DB::select("SELECT gcash_account_name FROM hotel_reservations");
                 $particular = "Hotel";
                 $debit = "Cash";
                 $remark = "FULL";
-                $finance_eventdate = "2023-12-04";
-                $finance_amount = DB::select("SELECT Payment FROM hotel_reservations");
+                $finance_eventdate = "2023-04-16";
+                // $finance_amount = DB::select("SELECT Payment FROM hotel_reservations");
+                $finance_payee = DB::table('hotel_reservations')->select('Guest_Name')->first()->Guest_Name;
+                $finance_amount = DB::table('hotel_reservations')->select('Payment')->first()->Payment;
+
 
                 DB::table('hotel_reservations')->where('Booking_No', $bookno)->update(array('Payment_Status' => $stats, 'Booking_Status' => $stats2));
                 DB::table('novadeci_suites')->where('Room_No', $roomno)->update(array('Status' => $stats2));
@@ -160,8 +163,8 @@ class HotelController extends Controller
                 DB::insert('insert into housekeepings (Room_No, Booking_No, Facility_Type, Facility_Status, Front_Desk_Status) 
                 values (?, ?, ?, ?, ?)', [$roomno, $bookno, $facility, $stats2, $stats2]);
 
-                DB::insert('insert into finance_2_reports (ornum, payee, particular, debit, remark, amount ,eventdate) 
-                values (?, ?, ?, ?, ?, ?, ?)', [$ornum, $finance_payee, $particular, $debit, $remark, $finance_amount, $finance_eventdate]);
+                DB::insert('insert into finance_2_reports (ornum, payee, particular, debit, remark, amount ,eventdate ,created_at) 
+                values (?, ?, ?, ?, ?, ?, ?,now())', [$ornum, $finance_payee, $particular, $debit, $remark, $finance_amount, $finance_eventdate]);
 
         
                 Alert::Success('Success', 'Payment successfully updated!');
