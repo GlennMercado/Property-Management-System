010f80bacef2340dd743bd86420dbfd82255e098
Rraine Autor <Rraine@gmail.com>
2023-04-23 20:48:07 +0800

    operation > dashboard, event inq

--- resources/views/Admin/pages/OperationManagement/OperationDashboard.blade.php
+++ resources/views/Admin/pages/OperationManagement/OperationDashboard.blade.php
@@ -15,11 +15,11 @@
     <div class="container-fluid mt--8">
         <div class="row align-items-center py-4">
             <div class="col-lg-12 col-12">
-                <h6 class="h2 text-dark d-inline-block mb-0">Operation Management Dashboard</h6>
+                <h6 class="h2 text-dark d-inline-block mb-0">Operations Management Dashboard</h6>
                 <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                     <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-home"></i></a></li>
-                        <li class="breadcrumb-item">Operation Management</li>
+                        <li class="breadcrumb-item">Operations Management</li>
                         <li class="breadcrumb-item active text-dark" aria-current="page">Operation Management Dashboard</li>
                     </ol>
                 </nav>
