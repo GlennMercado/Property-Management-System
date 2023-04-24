0ddd69d83bcb2b19a6e8573eea35d894e0d7927d
maalqoh14 <glennlainardmercado@gmail.com>
2023-04-24 17:41:04 +0800

    notif fixed

--- resources/views/Guest/MyNotifications.blade.php
+++ resources/views/Guest/MyNotifications.blade.php
@@ -3,43 +3,15 @@
 @section('content')
     @include('layouts.navbars.navs.guestloggedin')
     <div class="container mt-8 content">
-        <h1>My Notifications</h1>
+        <h1><i class="bi bi-bell-fill"></i> My Notifications</h1>
         {{-- <h3>Suites</h3>
                 <h2><i class="bi bi-info-circle-fill text-success pr-2"></i>You have successfully booked a hotel</h2>
                 <p class="text-left text-muted pb-0 mb-0">5min ago</p> --}}
         @forelse (auth()->user()->notifications as $notif)
-            <a class="" href="{{ $notif->data['link'] }}">
+            <a href="{{ $notif->data['link'] }}">
                 <div class="shadow rounded card-hover p-4 m-2">
                     @if ($notif->data['link'])
-                        <h3 class="text-truncate"><i class="bi bi-info-circle-fill text-success pr-2"></i>{{ $notif->data['txt'] }}</h3>
-                    @endif
-                </div>
-            </a>
-            <a class="" href="{{ $notif->data['link'] }}">
-                <div class="shadow rounded card-hover p-4 m-2">
-                    @if ($notif->data['link'])
-                        <h3 class="text-truncate"><i class="bi bi-info-circle-fill text-success pr-2"></i>{{ $notif->data['txt'] }}</h3>
-                    @endif
-                </div>
-            </a>
-            <a class="" href="{{ $notif->data['link'] }}">
-                <div class="shadow rounded card-hover p-4 m-2">
-                    @if ($notif->data['link'])
-                        <h3 class="text-truncate"><i class="bi bi-info-circle-fill text-success pr-2"></i>{{ $notif->data['txt'] }}</h3>
-                    @endif
-                </div>
-            </a>
-            <a class="" href="{{ $notif->data['link'] }}">
-                <div class="shadow rounded card-hover p-4 m-2">
-                    @if ($notif->data['link'])
-                        <h3 class="text-truncate"><i class="bi bi-info-circle-fill text-success pr-2"></i>{{ $notif->data['txt'] }}</h3>
-                    @endif
-                </div>
-            </a>
-            <a class="" href="{{ $notif->data['link'] }}">
-                <div class="shadow rounded card-hover p-4 m-2">
-                    @if ($notif->data['link'])
-                        <h3 class="text-truncate"><i class="bi bi-info-circle-fill text-success pr-2"></i>{{ $notif->data['txt'] }}</h3>
+                        <h4 class="notif-text"><i class="bi bi-info-circle-fill text-success pr-2"></i>{{ $notif->data['txt'] }}</h4>
                     @endif
                 </div>
             </a>
@@ -59,4 +31,12 @@
     .card-hover:hover {
         background-color: aliceblue;
     }
+
+    .notif-text {
+    display: -webkit-box;
+    max-width: 850px;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+}
 </style>
