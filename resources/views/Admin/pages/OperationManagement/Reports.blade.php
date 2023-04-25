18e3b79aceee205e62e9517b8b75f265ddd26b9a
Rraine Autor <Rraine@gmail.com>
2023-04-25 19:21:23 +0800

    OM, GUEST > CHANGES

--- resources/views/Admin/pages/OperationManagement/Reports.blade.php
+++ resources/views/Admin/pages/OperationManagement/Reports.blade.php
@@ -20,7 +20,7 @@
                 <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                     <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-home"></i></a></li>
-                        <li class="breadcrumb-item">Operation Management</li>
+                        <li class="breadcrumb-item">Operations Management</li>
                         <li class="breadcrumb-item active text-dark" aria-current="page">Reports</li>
                     </ol>
                 </nav>
