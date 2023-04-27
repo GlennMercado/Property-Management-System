102dfa3ab3df7fa0f4bad52da0229dfbd845db20
maalqoh14 <glennlainardmercado@gmail.com>
2023-04-27 17:13:32 +0800

    diaps - admin account change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -32,8 +32,8 @@ return new class extends Migration
         DB::table('users')->insert(
             array(
                 'name' => 'SUPER ADMIN',
-                'email' => 'admin@gmail.com',
-                'password' => Hash::make('admin123'),
+                'email' => 'inter.sol@gmail.com',
+                'password' => Hash::make('A4rglujuforvih19'),
                 'User_Type' => 'Admin',
             )
         );
