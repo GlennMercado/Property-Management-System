b7121fb1e139fb6369b40ac5194ba8901c44608e
jhoril <jhoril.gambong.00@gmail.com>
2023-05-01 16:42:36 +0800

    diaps - Security deposit in maintenance

--- resources/views/Admin/pages/CommercialSpaces/Mail/comm_maintenance2.blade.php
+++ resources/views/Admin/pages/CommercialSpaces/Mail/comm_maintenance2.blade.php
@@ -8,6 +8,11 @@
         <p>Hello {{ $tenant->name_of_owner }},</p>
         <p>We would like to inform you that your unit maintenance payment has been checked.</p>
         <p>Regards,<br>NOVADECI PROPERTIES</p>
+    @elseif($status == "Security Deposit")
+        <p>Hello {{ $tenant->name_of_owner }},</p>
+        <p>We would like to inform you that we deducted the total cost of the maintenance of your unit from your Security Deposit.</p>
+        <p>Please login and check the details in our website ({{url('/')}})</p>
+        <p>Regards,<br>NOVADECI PROPERTIES</p>
     @else
         <p>Hello {{ $tenant->name_of_owner }},</p>
         <p>We would like to inform you that your unit maintenance payment is incorrect.</p>
