b8c2f88553a765e41959bff016c2775e814f0005
maalqoh14 <glennlainardmercado@gmail.com>
2023-05-05 13:05:48 +0800

    12312uyi3tf12u3hjk

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -482,8 +482,8 @@ class HotelController extends Controller
                         Carbon::createFromFormat('Y-m-d H:i:s', $issued_time);
 
 
-                        DB::insert('insert into Key_Management (Key_ID, Room_No, Booking_No, Attendant, Issued_Time, Due_Time) 
-                        values (?, ?, ?, ?, ?, ?)', [$keyid ,$roomno, $bookno, $attendant, $issued_time, $due_time]);
+                        // DB::insert('insert into Key_Management (Key_ID, Room_No, Booking_No, Attendant, Issued_Time, Due_Time) 
+                        // values (?, ?, ?, ?, ?, ?)', [$keyid ,$roomno, $bookno, $attendant, $issued_time, $due_time]);
 
                         Alert::Success('Success', 'Booking Successfully Checked Out!');
                         return redirect('HotelReservationForm')->with('Success', 'Data Saved');
@@ -535,8 +535,8 @@ class HotelController extends Controller
                         Carbon::createFromFormat('Y-m-d H:i:s', $issued_time);
 
 
-                        DB::insert('insert into Key_Management (Key_ID, Room_No, Booking_No, Attendant, Issued_Time, Due_Time) 
-                        values (?, ?, ?, ?, ?, ?)', [$keyid ,$roomno, $bookno, $attendant, $issued_time, $due_time]);
+                        // DB::insert('insert into Key_Management (Key_ID, Room_No, Booking_No, Attendant, Issued_Time, Due_Time) 
+                        // values (?, ?, ?, ?, ?, ?)', [$keyid ,$roomno, $bookno, $attendant, $issued_time, $due_time]);
 
                         Alert::Success('Success', 'Booking Successfully Checked Out!');
                         return redirect('HotelReservationForm')->with('Success', 'Data Saved');
