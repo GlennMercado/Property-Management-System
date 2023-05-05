02c3cbb466069967c49b416fdf1090bea22c84cc
maalqoh14 <glennlainardmercado@gmail.com>
2023-05-05 09:48:28 +0800

    diaps inayos lng ung email

--- app/Mail/BookingConfirmation.php
+++ app/Mail/BookingConfirmation.php
@@ -12,16 +12,18 @@ class BookingConfirmation extends Mailable
     use Queueable, SerializesModels;
 
     protected $name;
+    protected $stats2;
 
-    public function __construct($name)
+    public function __construct($name, $stats2)
     {
         $this->name = $name;
+        $this->stats2 = $stats2;
     }
 
     public function build()
     {
         return $this->subject('Booking Confirmation')
             ->view('Admin.pages.Reservations.Mail.BookingConfirmation')
-            ->with(['name' => $this->name]);
+            ->with(['name' => $this->name, 'stats2' => $this->stats2]);
     }
 }
